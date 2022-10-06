413d04bf9d6f94f1a730dab12cd3e9f9504c9cb2
Farkas Zoltán <farkasz@KKSZKI.LOCAL>
2022-10-06 13:10:41 +0200

    Javascript elkeszult.

---
+++ HÉV/hev.js
@@ -0,0 +1,16 @@
+function szamol()
+{
+    var indulo = document.getElementById("select1").value;
+    var erkezo = document.getElementById("select2").value;
+
+    var eredmeny=indulo-erkezo;
+
+   if(indulo==erkezo){
+    alert("Hiba: azonos megállókat választott ki!");
+   }
+   else{
+    alert("Menetidő: "+Math.abs(eredmeny)+" perc!");
+   }
+    
+   
+}
